83953962558421a3567be4613b2d704752afd7aa
kljh <code@main2.fr>
2021-08-29 16:07:00 +0100

    Clock v0.1

--- misc/laser/clock.js
+++ misc/laser/clock.js
@@ -7,8 +7,11 @@ function init() {
 function draw_clock() {
     var inputs = get_inputs();
     
-    // Dimenesions in mm
+    // Dimensions in mm
     var thickness = 5;
+    var servos_length = 30;
+    var servos_width = 30;
+
     var base_length = 200;
     var base_width = 30;
     var base_rcorner = 10;
@@ -26,8 +29,44 @@ function draw_clock() {
     var track_gap = 5;
     var track_rout = 5;
     var track_rin = 4;
-    
+
     var cut = document.getElementById("cut");
+
+    var g995 = defs.appendChild(svg_node("g", { id: "mg995" })), r = 1.5;
+    g995.appendChild(svg_node("rect", { x: -22, y: -10.5, width: 44, height: 21 }));
+    g995.appendChild(svg_node("circle", { cx: -24.5, cy: -5, r }));
+    g995.appendChild(svg_node("circle", { cx: -24.5, cy: +5, r }));
+    g995.appendChild(svg_node("circle", { cx: +24.5, cy: -5, r }));
+    g995.appendChild(svg_node("circle", { cx: +24.5, cy: +5, r }));
+    var g = defs.appendChild(svg_node("g", { id: "servos" }));
+    g.appendChild(svg_node("rect", { x: 0, y: 0, width: 160, height: 30, rx: 5, ry: 5 }));
+    g.appendChild(svg_node("use", { "xlink:href": "#mg995", transform: `translate(${40} ${15})` }));
+    g.appendChild(svg_node("use", { "xlink:href": "#mg995", transform: `translate(${120} ${15})` }));
+    
+    var g = defs.appendChild(svg_node("g", { id: "servo_arm_1" }));
+    g.appendChild(svg_node("rect", { x: 0, y: 0, width: 100, height: 8, rx: 4, ry: 4 }));
+    g.appendChild(svg_node("circle", { cx: 5, cy: 4, r: 0.8 }));
+    g.appendChild(svg_node("circle", { cx: 5+26.5, cy: 4, r: 0.8 }));
+    g.appendChild(svg_node("circle", { cx: 5+26.5/2, cy: 4, r }));
+    for (var i=0; i<6; i++) 
+        g.appendChild(svg_node("circle", { cx: 10*i+45, cy: 4, r }));
+
+    var g = defs.appendChild(svg_node("g", { id: "servo_arm_2" }));
+    g.appendChild(svg_node("rect", { x: 0, y: 0, width: 100, height: 8, rx: 4, ry: 4 }));
+    for (var i=0; i<10; i++) 
+        g.appendChild(svg_node("circle", { cx: 10*i+5, cy: 4, r }));
+    
+    var g = defs.appendChild(svg_node("g", { id: "servo_rest" }));
+    g.appendChild(svg_node("circle", { cx: 0, cy: 0, r }));
+    g.appendChild(svg_node("circle", { cx: 0, cy: 0, r: 25 }));
+    g.appendChild(svg_node("line", { x1: -25, y1: -10, x2: 25, y2: -10 }));
+    g.appendChild(svg_node("path", { d: "M -21.5 0 A 21.5 21.5 0 0 0 21.5 0 L 18.5 0 A 18.5 18.5 0 0 1 -18.5 0 Z" }));
+
+    var g = defs.appendChild(svg_node("g", { id: "guards" }));
+    var ri = 5.0, ro = 6.5, l = 6;
+    var d = `M ${-l} ${ri} L 0 ${ri} A ${ri} ${ri} 0 0 0 0 ${-ri} L ${-l} ${-ri} L ${-l} ${-ro} L 0 ${-ro} 
+     A ${ro} ${ro} 0 0 1 0 ${ro} L ${-l} ${ro} Z` ;
+    g.appendChild(svg_node("path", { d }));
     
     var g = defs.appendChild(svg_node("g", { id: "base" }));
     g.appendChild(svg_node("rect", { x: -base_length/2, y: -base_width/2, width: base_length, height: base_width, rx: base_rcorner, ry: base_rcorner }));
@@ -66,6 +105,14 @@ function draw_clock() {
     
  
     var y0 = 0;
+    cut.appendChild(svg_node("use", { "xlink:href": "#servo_rest", transform: `translate(${200} ${y0})` }));
+    cut.appendChild(svg_node("use", { "xlink:href": "#guards", transform: `translate(${200} ${y0+40})` }));
+    cut.appendChild(svg_node("use", { "xlink:href": "#servos", transform: `translate(${0} ${y0})` }));
+    y0 += servos_width + 2;
+    cut.appendChild(svg_node("use", { "xlink:href": "#servo_arm_1", transform: `translate(${0} ${y0})` }));
+    y0 += 10;
+    cut.appendChild(svg_node("use", { "xlink:href": "#servo_arm_2", transform: `translate(${0} ${y0})` }));
+    y0 += 10;
     cut.appendChild(svg_node("use", { "xlink:href": "#base", transform: `translate(${base_length/2} ${y0 + base_width/2})` }));
     y0 += base_width + 2;
     cut.appendChild(svg_node("use", { "xlink:href": "#base", transform: `translate(${base_length/2} ${y0 + base_width/2})` }));
